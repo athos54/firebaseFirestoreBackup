9c924422a23b5a60d123661a96c5ac188b8fca8a
athos <athos.oc@gmail.com>
2020-05-18 13:03:09 +0200

    modify to handle huge collections

--- src/app/backupDB.js
+++ src/app/backupDB.js
@@ -49,28 +49,48 @@ async function main() {
 
 function saveCollection(coleccion, destinyPath) {
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async(resolve, reject) => {
+        let isFinish = false
+        const step = 500
+        let offset = -step
+        let data = [];
 
-        admin.firestore().collection(coleccion).get().then(docs => {
-            
-            let data = [];
-            
-            for (let i = 0; i < docs.docs.length; i++) {
+        while (isFinish === false){
+            console.log("data.length", data.length)
+            offset = offset + step
+            console.log("offset ", offset, " | ", step)
+            await admin.firestore().collection(coleccion).offset(offset).limit(step).get().then(docs => {
+                // console.log("docs.docs", docs.docs)
+                if(docs.docs.length === 0){
+                    console.log('Coleccion finalizada');
+                    isFinish=true
+                }else{
+                    
+                    console.log(`The collection ${coleccion} has ${docs.docs.length} documents`, docs.docs.length)
+        
+                    for (let i = 0; i < docs.docs.length; i++) {
+        
+                        const element = docs.docs[i];
+                        let aux = element.data();
+                        aux.idDoc = element.id;
+                        data.push(aux);
+        
+                    }
+                    
+                }
+                
+                
+    
+            }).catch(err=>{ reject(err); });
+    
+        }
+        console.log("Starting to save " + coleccion);
+        const filenamepath = path.join(destinyPath, coleccion + '.json');
+        fs.writeFileSync(filenamepath, JSON.stringify(data), { encoding: 'utf8' });
+        console.log(coleccion + " saved");
 
-                const element = docs.docs[i];
-                let aux = element.data();
-                aux.idDoc = element.id;
-                data.push(aux);
-
-            }
-
-            const filenamepath = path.join(destinyPath, coleccion + '.json');
-            fs.writeFileSync(filenamepath, JSON.stringify(data), { encoding: 'utf8' });
-
-            resolve();
-
-        }).catch(err=>{ reject(err); });
-
+        resolve();
+        
     });
 }
 
